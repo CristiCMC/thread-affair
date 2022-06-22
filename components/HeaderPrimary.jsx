13b314da04f4287ae6a018fc4868f29cbaac488d
CristiCMC <100723697+CristiCMC@users.noreply.github.com>
2022-06-22 20:05:38 +0100

    thread affair class work

--- components/HeaderPrimary.jsx
+++ components/HeaderPrimary.jsx
@@ -1,3 +1,61 @@
 export default function HeaderPrimary() {
-  return <section className="header-primary">header primary</section>
+  return (
+    <section className="header-primary d-none d-lg-block">
+      <nav className="nav-primary">
+        <ul>
+          <li>
+            <a href="https://x.ro" title="Home">
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="https://x.ro" title="Costume">
+              Costume
+            </a>
+          </li>
+
+          <li>
+            <a href="https://x.ro" title="Camasi">
+              Camasi
+            </a>
+          </li>
+
+          <li>
+            <a href="https://x.ro" title="Accesorii">
+              Accesorii
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="nav-secondary">
+        <ul>
+          <li>
+            <a href="http://x.ro" title="Glamour">
+              Glamour
+            </a>
+          </li>
+
+          <li>
+            <a href="http://x.ro" title="Wedding Suits">
+              Wedding Suits
+            </a>
+          </li>
+
+          <li>
+            <a href="http://x.ro" title="Office Suits">
+              Office Suits
+            </a>
+          </li>
+
+          <li>
+            <a href="http://x.ro" title="Customize your suit">
+              Customize your suit
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
 }
